6be92e10ee3a480f46abe7785ac0e49ed7cd5cfb
CarlosPerez <carlosmperez2901@gmail.com>
2025-06-11 17:43:11 -0700

    Add set function to add entries to the Hash Map

---
+++ HashMap.js
@@ -0,0 +1,131 @@
+import { LinkedList } from './LinkedList.js';
+
+export class HashMap {
+  #loadFactor = 0.9;
+  #capacity = 16; // Number of buckets
+  buckets = new Array(16);
+
+  hash(key) {
+    // Take a key and produces a hash code with it
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    }
+
+    return hashCode;
+  }
+
+  #getBucketIndex(key) {
+    const hashCode = this.hash(key);
+    const index = hashCode % this.#capacity;
+
+    return index;
+
+  }
+
+  set(key, value) {
+    // Creates a new key-value pair. 
+    // If the key already exits just update its value
+
+    // console.log(this.buckets.length * this.#loadFactor)
+
+    const index = this.#getBucketIndex(key)
+    const newPair = new Node(key, value);
+    const nodesList = this.buckets[index]
+    // Create a linked List to avoid future collisions 
+    const newNodesList = new LinkedList();
+
+    // If there is nothing in the bucket
+    if (!this.buckets[index]) {
+      // Add the new key-pair value to the list
+      newNodesList.append(newPair);
+      // Associate that bucket to the list and its elements
+      this.buckets[index] = newNodesList;
+      return;
+    }
+
+    let node = nodesList.head;
+    // If There is at least one thing in the bucket
+    while (node) {
+      // Check if the node in the bucket is what we're looking for
+      if (node.key === newPair.key) {
+        // If it is, add the new key-value pair to the new list
+        newNodesList.append(newPair);
+      } else {
+        // If the node is not the one we're looking for
+        // Add the rest of the nodes to the list so we don't loose data
+        newNodesList.append(node);
+        // Move the cursor to the next node to repeat the process
+        let nextNode = node.nextNode;
+        node = nextNode;
+      }
+    }
+
+    this.buckets[index] = newNodesList;
+
+  }
+
+  get(key) {
+    // Returns the value assigned to that key.
+    // If a key is not found, return null
+    const index = this.#getBucketIndex(key);
+    return this.buckets[index];
+  }
+
+  has(key) {
+    // Returns true or false based on whether or not the key is in the hash map
+    const index = this.#getBucketIndex(key);
+    return this.buckets[index] ? true : false;
+  }
+
+  remove(key) {
+    // Remove the entry with that key and return true. Otherwise false.
+    const index = this.#getBucketIndex(key);
+    const entry = this.buckets[index];
+    if (entry) {
+      delete this.buckets[index];
+      return true;
+    }
+    return false;
+  }
+
+  get length() {
+    // Returns the number of keys stored in the hash map
+  }
+
+  clear() {
+    // Remove all entries in the hash map
+    this.buckets = new Array(16)
+  }
+
+  keys() {
+    // Returns an array containing all the keys inside the hash map
+    const keys = [];
+    for (let key of this.buckets) {
+      keys.push(key);
+    }
+    return keys;
+  }
+  values() {
+    // Returns an array containing all the values
+  }
+
+  entries() {
+    // Returns an array that contains each key,value pair.
+    // Example: [[firstKey, firstValue], [secondKey, secondValue]]
+  }
+
+}
+
+class Node {
+  key;
+  value;
+  nextNode = null;
+
+  constructor(key, value) {
+    this.key = key;
+    this.value = value;
+  }
+}
---
+++ LinkedList.js
@@ -0,0 +1,127 @@
+
+export class LinkedList {
+  #list = [];
+  #size = 0;
+
+  append(node) {
+    // Adds a new node containing @value to the end of the list
+    if (this.#size !== 0) {
+      const lastNode = this.at(this.#size - 1);
+      lastNode.nextNode = node;
+    }
+    this.#list[this.#size] = node;
+    this.#size ++;
+  }
+
+  prepend(node) {
+    // Adds a new node containing @value to the start of the list
+    node.nextNode = this.head;
+    this.#list[0] = node;
+    this.#size ++;
+  }
+
+  get size() {
+    // Returns the total number of nodes in the list
+    return this.#size;
+  }
+
+  get head() {
+    // Returns the first node in the list
+    return this.#list[0];
+  }
+
+  get tail() {
+    // Returns the last node in the list
+    return this.at(this.#size - 1);
+  }
+
+  at(index) {
+    // Returns the node at the given index
+    let node = this.head;
+    for (let i = 0; i < index; i++) {
+      let nextNode = node.nextNode;
+      node = nextNode;
+    }
+    return node;
+  }
+
+  pop() {
+    // Removes the last element from the list
+
+    let currentNode = this.head;
+    // Iterates until the the node pointing to the tail is found
+    while (currentNode.nextNode !== this.tail) {
+      currentNode = currentNode.nextNode;
+    }
+    // Unlink the nodes
+    currentNode.nextNode = null;
+    this.#size --;
+
+
+  }
+
+  contains(value) {
+    // Returns true if the passed in value is in the list
+    // and otherwise returns false.
+    let currentNode = this.head;
+    for (let i = 0; i < this.#size; i++) {
+      if (currentNode.value === value) return true;
+      let nextNode = currentNode.nextNode;
+      currentNode = nextNode;
+    }
+    return false;
+  }
+
+  find(value) {
+    // Returns the index of the node containing value, or null if not found.
+    let currentNode = this.head;
+    for (let i = 0; i < this.#size; i++) {
+      if (currentNode.value === value) return i;
+      let nextNode = currentNode.nextNode;
+      currentNode = nextNode;
+    }
+    return null;
+  }
+
+  toString() {
+    // represents your LinkedList objects as strings,
+    // so you can print them out and preview them in the console.
+    // The format should be: ( value ) -> ( value ) -> ( value ) -> null
+    let stringList = '';
+    let currentNode = this.head;
+
+    while (currentNode !== null) {
+      stringList += `( key: ${currentNode.key}  value: ${currentNode.value}) -> `;
+      let nextNode = currentNode.nextNode;
+      currentNode = nextNode;
+    }
+    return stringList += 'null';
+  }
+
+  insertAt(node, index) {
+    // That inserts a new node with the provided @value at the given @index.
+    // Follows the following structure:
+    // Previous structure: (previousNode) -> (nextNode)
+    // Resulting structure: (previousNode) -> (newNode) -> (nextNode)
+    // Get the node previous to the index postiion
+    // so we can link it to the new Node 
+    let previousNode = this.at(index - 1);
+    node.nextNode = previousNode.nextNode;
+    // Relink the prevous node to the new node
+    previousNode.nextNode = node;
+
+    this.#size ++;
+  }
+  removeAt(index) {
+    // Removes the node at the given @index.
+
+    const previousNode = this.at(index - 1);
+    // Link the previous node to the element linked with
+    // the element wants to be deleted.
+    // before: (prevNode) -> (nodeToDelete) -> (nextNode)
+    // result: (prevNode) -> (nextNode)
+    previousNode.nextNode = this.at(index + 1);
+    this.#size --;
+  }
+
+}
